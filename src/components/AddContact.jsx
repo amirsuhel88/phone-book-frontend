0ace84c47d25aa2bb4630bbe2b8192dc2f5211a9
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-08-17 02:12:34 +0530

    contact is saving with image and also fetching image with other info. logut is laso implemented

---
+++ src/components/AddContact.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import { Row, Alert, Button, Form, Container, Col } from "react-bootstrap";
+import axios from "axios";
+
+function AddContact() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [photo, setPhoto] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("token");
+
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("phone", phone);
+      formData.append("email", email);
+      formData.append("photo", photo);
+
+      const response = await axios.post(
+        "http://localhost:5000/api/contacts/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Container>
+      <h2 className="my-4">Add Contact</h2>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm="2">
+            Name
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm="2">
+            Phone
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control
+              type="text"
+              placeholder="Enter Phone number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm="2">
+            Email
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control
+              type="text"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm="2">
+            Image
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control
+              type="file"
+              accept="image/*"
+              onChange={(e) => setPhoto(e.target.files[0])}
+            />
+          </Col>
+        </Form.Group>
+        <Button variant="primary" type="submit" disabled={status === "loading"}>
+          {status === "loading" ? "Saving..." : "Save"}
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default AddContact;
