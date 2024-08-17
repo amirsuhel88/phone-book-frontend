3c81d37e78cc670adaed17883ce9107825fd51cf
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-08-17 14:00:05 +0530

    login bug fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { setToken, setStatus, setError, loginUser } from "../redux/userSlice";
 import { Alert, Button, Form, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -23,33 +22,39 @@ const LoginPage = () => {
 
   return (
     <Container>
-      <h2 className="my-4">Login</h2>
-      {status === "failed" && <Alert variant="danger">{error}</Alert>}
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" disabled={status === "loading"}>
-          {status === "loading" ? "Logging in..." : "Login"}
-        </Button>
-      </Form>
+      <div className="col-6 mx-auto p-4 border border-danger" id="login_form">
+        <h2 className="my-4">Login</h2>
+        {status === "failed" && <Alert variant="danger">{error}</Alert>}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="submit"
+            disabled={status === "loading"}
+          >
+            {status === "loading" ? "Logging in..." : "Login"}
+          </Button>
+        </Form>
+      </div>
     </Container>
   );
 };
